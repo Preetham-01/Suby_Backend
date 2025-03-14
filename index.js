8f59aefa23b81057dfc1e3d76387bf6eaeb18dbe
Preetham <preethamseelam01@gmail.com>
2025-03-14 22:13:47 +0530

    added path module

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const vendorRoutes = require('./routes/vendorRoutes');
 const bodyParser = require('body-parser')
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
-//const cors = require('cors');
+const cors = require('cors');
 const path = require('path')
 
 const app = express()
 
+
 const PORT = 4000 || process.env.PORT;
 
 dotenv.config();
+app.use(cors())
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected Successfully"))
